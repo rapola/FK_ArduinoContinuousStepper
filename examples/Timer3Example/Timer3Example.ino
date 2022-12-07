6e4b6af53e82db747bee1a9af1a256c3f4bf8215
Ralf <59847860+rapola@users.noreply.github.com>
2022-12-07 16:13:55 +0100

    added function
    steps are counted:
     uint32_t getCntval()

--- examples/Timer3Example/Timer3Example.ino
+++ examples/Timer3Example/Timer3Example.ino
@@ -1,9 +1,10 @@
 // This example requires the TimerThree library.
 // https://github.com/PaulStoffregen/TimerThree
 
-#include <ContinuousStepper_Timer3.h>
-
-const uint8_t stepPin = 2;
+//#include <ContinuousStepper_Timer3.h>
+#include "src/ContinuousStepper_Timer3.h"
+//const uint8_t stepPin = 2;
+const uint8_t stepPin = 13;
 const uint8_t dirPin = 3;
 
 ContinuousStepper_Timer3 stepper;
@@ -16,4 +17,4 @@ void setup() {
 void loop() {
   // no need to call stepper.loop()
   // it's called by the timer interrupt handler
-}
+}}
